c48caa0692ccca29453135106cb51a958366ca43
Garrit <contact@garritschaap.com>
2015-02-06 13:26:38 +0700

    Initial Commit

---
+++ FUX/FUX.h
@@ -0,0 +1,19 @@
+//
+//  FUX.h
+//  FUX
+//
+//  Created by Garrit Schaap on 06.02.15.
+//  Copyright (c) 2015 Garrit UG (haftungsbeschränkt). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FUX.
+FOUNDATION_EXPORT double FUXVersionNumber;
+
+//! Project version string for FUX.
+FOUNDATION_EXPORT const unsigned char FUXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FUX/PublicHeader.h>
+
+
