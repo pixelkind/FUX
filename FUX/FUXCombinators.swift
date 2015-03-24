2bbc44dfdbc85110de682d0c2290b05717740c26
Garrit <contact@garritschaap.com>
2015-03-24 17:00:47 +0100

    Added BackgroundColor Value

--- FUX/FUXCombinators.swift
+++ FUX/FUXCombinators.swift
@@ -102,3 +102,17 @@ public func viewSizeValue(#view: UIView, #to: CGSize) -> FUXValue {
         view.frame.size.width = from.size.width + change.width * CGFloat(tweenValue)
         view.frame.size.height = from.size.height + change.height * CGFloat(tweenValue) })
 }
+
+public func backgroundColor(#view: UIView, #to: UIColor) -> FUXValue {
+    let from = view.backgroundColor!
+    return FUXValue.Value({ tweenValue in
+        let fromColors = CGColorGetComponents(from.CGColor);
+        let toColors = CGColorGetComponents(to.CGColor);
+        
+        let red = CGFloat(Float(fromColors[0]) * (1 - tweenValue) + Float(toColors[0]) * tweenValue)
+        let green = CGFloat(Float(fromColors[1]) * (1 - tweenValue) + Float(toColors[1]) * tweenValue)
+        let blue = CGFloat(Float(fromColors[2]) * (1 - tweenValue) + Float(toColors[2]) * tweenValue)
+        let alpha = CGFloat(Float(fromColors[3]) * (1 - tweenValue) + Float(toColors[3]) * tweenValue)
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    })
+}
